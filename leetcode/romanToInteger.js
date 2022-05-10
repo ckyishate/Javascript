0465d01a3c10b1612fe9c243335e208c9dc592d0
Ssentamu Abel <ssentamuabel90@gmail.com>
2022-05-10 09:33:58 +0300

    ejs & leetcode

--- leetcode/romanToInteger.js
+++ leetcode/romanToInteger.js
@@ -1,4 +1,8 @@
-var romanToInt = function(s) {
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var romanToInt = function(s) {
     
     let sArray = s.split("");
     let dic = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000 };
@@ -6,20 +10,18 @@ var romanToInt = function(s) {
     sArray.forEach((e)=>{
         if(e in dic) numArray.push(dic[e]);
     });
-    console.log(numArray);
+    
     let decimalNumber=0;
     for(let i= numArray.length-1; i>=0; i--){
         
-        if(numArray[i] >= decimalNumber ){
-            decimalNumber = decimalNumber + numArray[i];
-        
-        }else{
+        if( decimalNumber > numArray[i]){
+            decimalNumber += numArray[i];
+        }else
             decimalNumber -= numArray[i];
-        }
-        return decimalNumber;      
+        
+        console.log(decimalNumber)
     }
-    
-    
+    return decimalNumber;
     
 };
 
---
+++ nodeProjects/project1/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// REGISTER VIEW ENGINE
+app.set('view engine', 'ejs');
+
+//listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+
+    const blogs=[
+        // {title: 'Yoshi fins eggs', snippet:'todayy uis the day the supervisor is coming'},
+        // {title: 'Yoshi fins eggs', snippet:'todayy uis the day the supervisor is coming'},
+        // {title: 'Yoshi fins eggs', snippet:'todayy uis the day the supervisor is coming'},
+        // {title: 'Yoshi fins eggs', snippet:'todayy uis the day the supervisor is coming'}
+    ]
+
+    // res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+    // res.sendFile('/views/about.html', {root: __dirname} );
+    res.render('about', { title: 'about' });
+});
+
+//redirecting a page 
+
+app.get('/blogs/create', (req, res) => {
+
+    res.render('create', { title: 'create a new blog' });
+});
+
+//404 page not found
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
